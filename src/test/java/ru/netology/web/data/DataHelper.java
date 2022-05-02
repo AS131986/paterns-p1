96ea32bce29af05306457f255d29df829ed7ff06
Alexander Sklyar <AlexanderSGSklyar@mail.ru>
2022-05-02 19:55:22 +0300

    first

---
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -0,0 +1,71 @@
+package ru.netology.web.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataHelper {
+    private DataHelper() {}
+
+    private static Faker faker;
+
+    @BeforeAll
+    static void setUpAll() { faker = new Faker(new Locale("ru")); }
+
+    public static String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String planningDate = generateDate(7);
+
+
+
+    public String generateDate2 (int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String planningDate2 = generateDate2(9);
+
+    @Value
+   public static class FormInfo {
+        private String city;
+        private String date;
+        private String fullName;
+        private String phoneNumber;
+
+        public FormInfo(String city, String date, String fullName, String phoneNumber) {
+            this.city = city;
+            this.date = date;
+            this.fullName = fullName;
+            this.phoneNumber = phoneNumber;
+        }
+
+
+        public static FormInfo getFormInfo() {
+            faker = new Faker(new Locale("ru"));
+            String city = faker.address().city();
+            String fullName = faker.name().fullName();
+            String date = generateDate(7);
+            String phoneNumber = faker.phoneNumber().phoneNumber();
+            return new FormInfo( city, date, fullName, phoneNumber);}
+
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/web/page/FormPage.java
@@ -0,0 +1,45 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.Condition;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.Keys;
+import ru.netology.web.data.DataHelper;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+
+
+public class FormPage {
+
+    private static Faker faker;
+
+    @BeforeAll
+    static void setUpAll() { faker = new Faker(new Locale("ru")); }
+
+    static String generateDate (int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String planningDate = generateDate(7);
+
+    public void sendValidForm(DataHelper.FormInfo formInfo) {
+        $x("//input[@placeholder='Город']").val(formInfo.getCity());
+        $x("//input[@placeholder='Дата встречи']").click();
+        $x("//input[@placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder='Дата встречи']").val(formInfo.getDate());
+        $x("//input[@name='name']").val(formInfo.getFullName());
+        $x("//input[@name='phone']").val(formInfo.getPhoneNumber());
+        $x("//*[@class='checkbox__box']").click();
+        $(withText("Запланировать")).click();
+        $x("//*[contains(text(),'Успешно!')]").should(Condition.appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
+
+    }
+}
